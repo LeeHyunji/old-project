19f91a94bed3ac3a78607b32c1ea9fb7b80ea542
LeeHyunji <hyunji015@gmail.com>
2015-10-07 19:08:37 +0900

    Mobile Programming 5

--- src/kr/ac/embedded/kookmin/sales/DiscountSale.java
+++ src/kr/ac/embedded/kookmin/sales/DiscountSale.java
@@ -11,7 +11,8 @@ public class DiscountSale extends Sale {
 	private double	discount;	// A percent of the price. Cannot be negative.
 								
 	public DiscountSale() {
-		/** 구현 하시오 **/ 
+		discount = 0;
+		
 	}
 	
 	/**
@@ -19,11 +20,18 @@ public class DiscountSale extends Sale {
 	 * theDiscount is expressed as a percent of the price and is nonnegative.
 	 */
 	public DiscountSale(String theName, double thePrice, double theDiscount) {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/ 
+		setName(theName);
+		setPrice(thePrice);
+		setDiscount(theDiscount);
 	}
 	
 	public DiscountSale(DiscountSale originalObject) {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/ 
+		if (originalObject == null) {
+			System.out.println("Error: null Sale object.");
+			System.exit(0);
+		}
 	}
 	
 	public static void announcement() {
@@ -31,18 +39,27 @@ public class DiscountSale extends Sale {
 	}
 	
 	public double bill() {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/
+		return getPrice()*(100-discount)*0.01;
+		
 	}
 	
 	public double getDiscount() {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/ 
+		return discount;
 	}
 	
 	/**
 	 * Precondition: Discount is nonnegative.
 	 */
 	public void setDiscount(double newDiscount) {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/ 
+		if (newDiscount >= 0)
+			discount = newDiscount;
+		else {
+			System.out.println("Error: Negative price.");
+			System.exit(0);
+		}
 	}
 	
 	public String toString() {
@@ -50,11 +67,22 @@ public class DiscountSale extends Sale {
 	}
 	
 	public boolean equals(Object otherObject) {
-		/** 구현 하시오 **/ 
+		/** 援ы쁽 �븯�떆�삤 **/ 
+		if (otherObject == null)
+			return false;
+		else if (getClass() != otherObject.getClass())
+			return false;
+		else {
+			DiscountSale otherSale = (DiscountSale) otherObject;
+		return (bill() == otherSale.getPrice());
+			
+		}
 	}
 	
 	
 	public DiscountSale clone() {
-		/** 구현 하시오....  임시생성자 사용 **/ 
+		/** 援ы쁽 �븯�떆�삤....  �엫�떆�깮�꽦�옄 �궗�슜 **/ 
+		return new DiscountSale(this);
+	
 	}
 }
