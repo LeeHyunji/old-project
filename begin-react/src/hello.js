dbdd5b3813d40d4076e105e5080f5cbd66cb4952
LeeHyunji <hyunji015@gmail.com>
2021-08-08 00:33:05 +0900

    begin-react : jsx 익히기

--- begin-react/src/hello.js
+++ begin-react/src/hello.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Hello(){
-    return <div>안녕하세요</div>;
+    const name = "Hyunji";
+    const style = {
+        fontSzie : 40,
+        backgroundColor : "aqua",
+        padding: "1rem"
+    };
+    return ( 
+        <>
+            {/* 주석입니다. */}
+            <h1 style={style}>Hello World</h1>
+            <div //class를 사용할땐 className으로 써주기
+            className="introduce">{name}, 안녕하세요</div>
+        </>
+    );
 }
 
 export default Hello;
